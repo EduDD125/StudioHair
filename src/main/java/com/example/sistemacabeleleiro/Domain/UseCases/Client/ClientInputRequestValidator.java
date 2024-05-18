3d413158c7e6a0c861dd6eeef28f58dbe94accb6
Eduardo Derisso <dudu.derisso@gmail.com>
2024-05-18 15:20:04 -0300

    feat: create exception for validation of cliente input request

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Client/ClientInputRequestValidator.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Client/ClientInputRequestValidator.java
@@ -1,4 +1,29 @@
 package com.example.sistemacabeleleiro.Domain.UseCases.Client;
 
-public class ClientInputRequestValidator {
+import com.example.sistemacabeleleiro.Domain.Entities.Client.Client;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.Notification;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.Validator;
+
+public class ClientInputRequestValidator extends Validator<Client> {
+
+    @Override
+    public Notification validate(Client client) {
+        Notification notification = new Notification();
+
+        if (client == null) {
+            notification.addError("Client is null");
+            return notification;
+        }
+
+        if(nullOrEmpty(client.getName()))
+            notification.addError("Name is null or empty");
+        if(validEmail(client.getEmail()))
+            notification.addError("Email is not valid: " + client.getEmail().getValue());
+        if(nullOrEmpty(client.getPhone()))
+            notification.addError("phone is null or empty");
+        if(validCPF(client.getCpf()))
+            notification.addError("CPF is not valid: " + client.getCpf().getValue());
+
+        return notification;
+    }
 }
