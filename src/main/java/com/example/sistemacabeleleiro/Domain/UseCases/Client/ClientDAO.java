655272d9155f1dfdd4af57ed8b8578674ec95b1f
Eduardo Derisso <dudu.derisso@gmail.com>
2024-05-18 13:46:50 -0300

    feat: create Client DAO interface

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Client/ClientDAO.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Client/ClientDAO.java
@@ -1,4 +1,10 @@
 package com.example.sistemacabeleleiro.Domain.UseCases.Client;
 
-public class ClientDAO {
+import com.example.sistemacabeleleiro.Domain.Entities.Client.Client;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.DAO;
+
+import java.util.Optional;
+
+public interface ClientDAO extends DAO<Client, Integer> {
+    Optional<Client> findOneCPF(String cpf);
 }
