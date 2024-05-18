3dc627a77742cad80eaf6d1d9b00a93cf2ed4c07
Eduardo Derisso <dudu.derisso@gmail.com>
2024-05-18 15:20:30 -0300

    feat: add Use Case createCliente

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Client/CreateClientUseCase.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Client/CreateClientUseCase.java
@@ -1,4 +1,28 @@
 package com.example.sistemacabeleleiro.Domain.UseCases.Client;
 
+import com.example.sistemacabeleleiro.Domain.Entities.Client.Client;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.EntityAlreadyExistsException;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.Notification;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.Validator;
+
 public class CreateClientUseCase {
+    private ClientDAO clientDAO;
+
+    public CreateClientUseCase(ClientDAO clientDAO) {
+        this.clientDAO = clientDAO;
+    }
+
+    public String insert(Client client) {
+        Validator<Client> validator = new ClientInputRequestValidator();
+        Notification notification = validator.validate(client);
+
+        if(notification.hasErros())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        String cpf = client.getCpf().toString();
+        if(clientDAO.findOneCPF(cpf).isPresent())
+            throw new EntityAlreadyExistsException("This CPF is already in use.");
+
+        return clientDAO.create(client);
+    }
 }
