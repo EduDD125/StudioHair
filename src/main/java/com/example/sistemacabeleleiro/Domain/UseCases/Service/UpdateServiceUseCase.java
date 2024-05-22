3f244fe51683ca5b9b1df2cc9b8b9226e4f32790
Ariadne <ariadnederezende@gmail.com>
2024-05-22 10:55:10 -0300

    feat: added Update Service use case

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Service/UpdateServiceUseCase.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Service/UpdateServiceUseCase.java
@@ -1,4 +1,29 @@
 package com.example.sistemacabeleleiro.Domain.UseCases.Service;
 
+import com.example.sistemacabeleleiro.Domain.Entities.Service.Service;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.EntityAlreadyExistsException;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.EntityNotFoundException;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.Notification;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.Validator;
+
 public class UpdateServiceUseCase {
+    private ServiceDAO serviceDAO;
+
+    public UpdateServiceUseCase(ServiceDAO serviceDAO){
+            this.serviceDAO = serviceDAO;
+        }
+        public boolean update(Service service){
+            Validator<Service> validator = new ServiceInputRequestValidator();
+            Notification notification = validator.validate(service);
+
+            if(notification.hasErros())
+                throw new IllegalArgumentException(notification.errorMessage());
+
+            Integer id = service.getId();
+            if(serviceDAO.findOne(id).isEmpty())
+                throw new EntityNotFoundException("Service not found.");
+
+            return serviceDAO.update(service);
+        }
+
 }
