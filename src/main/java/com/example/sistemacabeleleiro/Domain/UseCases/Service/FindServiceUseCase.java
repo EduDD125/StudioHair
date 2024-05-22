8cf7d25a9aca1e7078d206c2e1dae20d1f34142b
Ariadne <ariadnederezende@gmail.com>
2024-05-22 15:48:06 -0300

    feat: added Find Service use case

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Service/FindServiceUseCase.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Service/FindServiceUseCase.java
@@ -1,4 +1,24 @@
 package com.example.sistemacabeleleiro.Domain.UseCases.Service;
 
+import com.example.sistemacabeleleiro.Domain.Entities.Service.Service;
+
+import java.util.List;
+import java.util.Optional;
+
 public class FindServiceUseCase {
+    private ServiceDAO serviceDAO;
+
+    public FindServiceUseCase(ServiceDAO serviceDAO) {
+        this.serviceDAO = serviceDAO;
+    }
+
+    public Optional<Service> findOne(Integer id){
+        if(id == null)
+            throw new IllegalArgumentException("ID can not be null.");
+        return  serviceDAO.findOne(id);
+    }
+
+    public List<Service> findAll(){
+        return serviceDAO.findAll();
+    }
 }
