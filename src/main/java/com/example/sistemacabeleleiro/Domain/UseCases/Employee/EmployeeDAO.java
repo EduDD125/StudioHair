a8631f2c3f9d3ef8c9f687d308b11778ad44dbbe
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-05-22 16:58:09 -0300

    feat: implementation of create employee use case

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Employee/EmployeeDAO.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Employee/EmployeeDAO.java
@@ -1,4 +1,13 @@
 package com.example.sistemacabeleleiro.Domain.UseCases.Employee;
 
-public interface EmployeeDAO {
+import com.example.sistemacabeleleiro.Domain.Entities.CPF.CPF;
+import com.example.sistemacabeleleiro.Domain.Entities.Employee.Employee;
+import com.example.sistemacabeleleiro.Domain.UseCases.Utils.DAO;
+
+import java.util.Optional;
+
+public interface EmployeeDAO extends DAO<Employee,Integer> {
+
+    Optional<Employee> findByCpf(CPF cpf);
+
 }
