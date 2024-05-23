97452f7805a8c63d33bfef65e72ddcdab06f9c92
Eduardo Derisso <dudu.derisso@gmail.com>
2024-05-23 12:23:02 -0300

    feat: add methods in ClientDAO to find client in diferente ways

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Client/ClientDAO.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Client/ClientDAO.java
@@ -3,10 +3,13 @@ package com.example.sistemacabeleleiro.Domain.UseCases.Client;
 import com.example.sistemacabeleleiro.Domain.Entities.Client.Client;
 import com.example.sistemacabeleleiro.Domain.UseCases.Utils.DAO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClientDAO extends DAO<Client, String> {
-    Optional<Client> findOneCPF(String cpf);
+    Optional<Client> findOneByCPF(String cpf);
+    Optional<Client> findOneByName(String name);
+    List<Client> findAll();
 
     String inactivate(Client client);
 }
