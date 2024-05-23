035f5f8cf3b5fe26b3b8f05ab35bee86f5ba73ca
fictorl <fictorlps@gmail.com>
2024-05-23 10:22:42 -0300

    feat: adding find methods

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Scheduling/SchedulingDAO.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Scheduling/SchedulingDAO.java
@@ -4,10 +4,15 @@ import com.example.sistemacabeleleiro.Domain.Entities.Employee.Employee;
 import com.example.sistemacabeleleiro.Domain.Entities.Schedulling.Scheduling;
 import com.example.sistemacabeleleiro.Domain.UseCases.Utils.DAO;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface SchedulingDAO extends DAO<Scheduling, Integer> {
     Optional<Scheduling> findByScheduledDate(LocalDateTime scheduledDate);
-    Optional<Scheduling> findByEmployee(Employee employee);
+    List<Scheduling> findByEmployee(Integer id);
+    List<Scheduling> findByClient(Integer id);
+    List<Scheduling> findByService(Integer id);
+    List<Scheduling> findByTimePeriod(LocalDate startDate, LocalDate endDate);
 }
