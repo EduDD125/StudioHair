0797fd30762a2809f3b3ba29e3ffc16e5b4e756e
Ariadne <ariadnederezende@gmail.com>
2024-05-28 16:12:02 -0300

    feat: added filter entities and filter methods

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Service/ServiceDAO.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Service/ServiceDAO.java
@@ -3,8 +3,13 @@ package com.example.sistemacabeleleiro.Domain.UseCases.Service;
 import com.example.sistemacabeleleiro.Domain.Entities.Service.Service;
 import com.example.sistemacabeleleiro.Domain.UseCases.Utils.DAO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ServiceDAO extends DAO<Service, Integer> {
     Optional<Service> findById(Integer id);
+    List<Service> findMostFrequent();
+    List<Service> findByPriceRange(double minPrice, double maxPrice);
+    List<Service> findByCategory(String category);
+    List<Service> findWithDiscount(Double discount);
 }
