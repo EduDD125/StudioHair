f6bbbe51472e27f7862ba0acc008461edf1d1a3f
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-05-29 01:09:46 -0300

    Fix: removal use cases for client and employee

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Client/RemoveClientUseCase.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Client/RemoveClientUseCase.java
@@ -1,6 +1,8 @@
 package com.example.sistemacabeleleiro.Domain.UseCases.Client;
 
+import com.example.sistemacabeleleiro.Domain.Entities.CPF.CPF;
 import com.example.sistemacabeleleiro.Domain.Entities.Client.Client;
+import com.example.sistemacabeleleiro.Domain.Entities.Employee.Employee;
 import com.example.sistemacabeleleiro.Domain.Entities.Schedulling.Scheduling;
 import com.example.sistemacabeleleiro.Domain.UseCases.Scheduling.SchedulingDAO;
 import com.example.sistemacabeleleiro.Domain.UseCases.Utils.EntityNotFoundException;
@@ -14,26 +16,38 @@ public class RemoveClientUseCase {
 
     private ClientDAO clientDAO;
 
-    public RemoveClientUseCase(ClientDAO clientDAO) {this.clientDAO = clientDAO;}
+    public RemoveClientUseCase(ClientDAO clientDAO, SchedulingDAO schedulingDAO) {
+        this.clientDAO = clientDAO;
+        this.schedulingDAO = schedulingDAO;
+    }
+
+    public boolean remove(Integer id) {
+        var client = clientDAO.findOne(id);
+        if (id == null || client.isEmpty())
+            throw new EntityNotFoundException("Client not found");
+
+        deleteClientSchedules(client.get());
+        return clientDAO.deleteByKey(id);
+    }
 
     public boolean remove(Client client) {
-        Validator<Client> validator = new ClientInputRequestValidator();
-        Notification notification = validator.validate(client);
-
-        if(notification.hasErros())
-            throw new IllegalArgumentException(notification.errorMessage());
 
         String cpf = client.getCpf().toString();
-        if (clientDAO.findOneByCPF(cpf).isEmpty())
+        if (client == null || clientDAO.findOneByCPF(cpf).toString().isEmpty())
             throw new EntityNotFoundException("This CPF isn´t registered");
 
-        List<Scheduling> schedulesToDelete = schedulingDAO.findAll().stream().
-                filter(schedule -> schedule.getClient().equals(client)).toList();
-        if(!schedulesToDelete.isEmpty()) {
-            for (Scheduling scheduling : schedulesToDelete) {
-                schedulingDAO.delete(scheduling);
+        deleteClientSchedules(client);
+        return clientDAO.delete(client);
+    }
+
+    private void deleteClientSchedules(Client client) {
+        List<Scheduling> schedulesToDelete = schedulingDAO.findAll().stream()
+                .filter(schedule -> schedule.getClient().equals(client))
+                .toList();
+        if (!schedulesToDelete.isEmpty()){
+            for (Scheduling schedule : schedulesToDelete) {
+                schedulingDAO.delete(schedule);
             }
         }
-        return clientDAO.delete(client);
     }
 }
--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Employee/RemoveEmployeeUseCase.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Employee/RemoveEmployeeUseCase.java
@@ -2,30 +2,54 @@ package com.example.sistemacabeleleiro.Domain.UseCases.Employee;
 
 import com.example.sistemacabeleleiro.Domain.Entities.Employee.Employee;
 import com.example.sistemacabeleleiro.Domain.Entities.Employee.EmployeeStatus;
+import com.example.sistemacabeleleiro.Domain.Entities.Schedulling.Scheduling;
+import com.example.sistemacabeleleiro.Domain.UseCases.Scheduling.SchedulingDAO;
 import com.example.sistemacabeleleiro.Domain.UseCases.Utils.EntityNotFoundException;
+
+import java.util.List;
 
 public class RemoveEmployeeUseCase {
     private EmployeeDAO employeeDAO;
+    private SchedulingDAO schedulingDAO;
 
-    public RemoveEmployeeUseCase(EmployeeDAO employeeDAO) {
+    public RemoveEmployeeUseCase(EmployeeDAO employeeDAO, SchedulingDAO schedulingDAO) {
         this.employeeDAO = employeeDAO;
+        this.schedulingDAO = schedulingDAO;
     }
     public boolean remove(Integer id){
-        if (id == null || employeeDAO.findOne(id).isEmpty()){
+        var employee = employeeDAO.findOne(id);
+        if (id == null || employee.isEmpty()){
             throw new EntityNotFoundException("Employee not found");
         }
-        if (employeeDAO.findOne(id).get().getStatus() != EmployeeStatus.INACTIVE){
-            throw new IllegalArgumentException("Can't delete an active employee");
-        }
+
+        validateEmployeeStatus(employee.get());
+        deleteEmployeeSchedules(employee.get());
         return employeeDAO.deleteByKey(id);
     }
     public boolean remove(Employee employee){
         if (employee == null || employeeDAO.findOne(employee.getId()).isEmpty()){
             throw new EntityNotFoundException("Employee not found");
         }
-        if (employeeDAO.findOne(employee.getId()).get().getStatus() != EmployeeStatus.INACTIVE){
+
+        validateEmployeeStatus(employee);
+        deleteEmployeeSchedules(employee);
+        return employeeDAO.delete(employee);
+    }
+
+    private void validateEmployeeStatus(Employee employee) {
+        if (employee.getStatus() != EmployeeStatus.INACTIVE) {
             throw new IllegalArgumentException("Can't delete an active employee");
         }
-        return employeeDAO.delete(employee);
+    }
+
+    private void deleteEmployeeSchedules(Employee employee) {
+        List<Scheduling> schedulesToDelete = schedulingDAO.findAll().stream()
+                .filter(schedule -> schedule.getEmployee().equals(employee))
+                .toList();
+        if (!schedulesToDelete.isEmpty()){
+            for (Scheduling schedule : schedulesToDelete) {
+                schedulingDAO.delete(schedule);
+            }
+        }
     }
 }
