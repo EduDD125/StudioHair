a413a4e89e22c28d577a518f49a3609c5ab74e6c
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-06-12 19:32:53 -0300

    feat: add logo to header report

--- src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Reports/ExportReportUseCase.java
+++ src/main/java/com/example/sistemacabeleleiro/Domain/UseCases/Reports/ExportReportUseCase.java
@@ -3,6 +3,8 @@ package com.example.sistemacabeleleiro.Domain.UseCases.Reports;
 import com.example.sistemacabeleleiro.Domain.Entities.Schedulling.Scheduling;
 import org.apache.pdfbox.pdmodel.*;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.util.Matrix;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -61,20 +63,35 @@ public class ExportReportUseCase {
             document.addPage(page);
 
             try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-                contentStream.beginText();
-                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-                contentStream.newLineAtOffset(50, 700);
+                float fontSize = 12;
+                contentStream.setFont(PDType1Font.HELVETICA_BOLD, fontSize);
+                PDImageXObject logo = PDImageXObject.createFromFile("src/main/resources/com/example/sistemacabeleleiro/logo/Logo.png", document);
+                float logoWidth = logo.getWidth() / 5;
+                float logoHeight = logo.getHeight() / 5;
+                float pageWidth = page.getMediaBox().getWidth();
+                float logoX = (pageWidth - logoWidth) / 2;
+                float logoY = page.getMediaBox().getHeight() - logoHeight - 20;
+                contentStream.drawImage(logo, logoX, logoY, logoWidth, logoHeight);
+
+                float startY = logoY - 50;
 
                 for (Scheduling scheduling : schedules) {
-                    contentStream.showText("Date: " + scheduling.getDataRealizacao().toString() +
+                    String text = "Date: " + scheduling.getDataRealizacao().toString() +
                             " | Employee: " + scheduling.getEmployee().getName() +
                             " | Service: " + scheduling.getService().getName() +
                             " | Client: " + scheduling.getClient().getName() +
-                            " | Value: " + scheduling.getService().getValueOfService().toString());
-                    contentStream.newLineAtOffset(0, -50); // Move the cursor down for the next line
-                }
+                            " | Value: " + scheduling.getService().getValueOfService().toString();
 
-                contentStream.endText();
+                    float textWidth = PDType1Font.HELVETICA.getStringWidth(text) / 1000 * fontSize;
+                    float textX = (pageWidth - textWidth) / 2;
+
+                    contentStream.beginText();
+                    contentStream.setTextMatrix(Matrix.getTranslateInstance(textX, startY));
+                    contentStream.showText(text);
+                    contentStream.endText();
+
+                    startY -= fontSize + 25;
+                }
             }
 
             document.save("src/main/resources/com/example/sistemacabeleleiro/reports/" + fileName + ".pdf");
