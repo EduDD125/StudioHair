c7cb839be089cee5af589c0e0afff2ad6957f6e3
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-06-19 21:03:15 -0300

    refactor: refactoring update service use case

--- src/main/java/com/example/sistemacabeleleiro/domain/usecases/service/UpdateServiceUseCase.java
+++ src/main/java/com/example/sistemacabeleleiro/domain/usecases/service/UpdateServiceUseCase.java
@@ -1,5 +1,6 @@
 package com.example.sistemacabeleleiro.domain.usecases.service;
 
+import com.example.sistemacabeleleiro.application.dtos.service.ServiceUpdateDTO;
 import com.example.sistemacabeleleiro.domain.entities.service.Service;
 import com.example.sistemacabeleleiro.domain.usecases.utils.EntityNotFoundException;
 import com.example.sistemacabeleleiro.domain.usecases.utils.Notification;
@@ -11,17 +12,17 @@ public class UpdateServiceUseCase {
     public UpdateServiceUseCase(ServiceDAO serviceDAO){
             this.serviceDAO = serviceDAO;
         }
-        public boolean update(Service service){
-            Validator<Service> validator = new ServiceInputRequestValidator();
-            Notification notification = validator.validate(service);
+        public boolean update(ServiceUpdateDTO serviceUpdateDTO){
+            Validator<ServiceUpdateDTO> validator = new ServiceUpdateRequestValidator();
+            Notification notification = validator.validate(serviceUpdateDTO);
 
             if(notification.hasErros())
                 throw new IllegalArgumentException(notification.errorMessage());
 
-            Integer id = service.getId();
-            if(serviceDAO.findOne(id).isEmpty())
-                throw new EntityNotFoundException("Service not found.");
+            int id = serviceUpdateDTO.id();
 
+            Service service = serviceDAO.findOne(id)
+                    .orElseThrow(()-> new EntityNotFoundException("Service not found"));
             return serviceDAO.update(service);
         }
 
