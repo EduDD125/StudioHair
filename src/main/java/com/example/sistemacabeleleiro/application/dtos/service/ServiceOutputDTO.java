09820ee4cb984c6d839e1b057ab66019479197d1
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-06-19 21:14:47 -0300

    refactor: refactoring find service use case

--- src/main/java/com/example/sistemacabeleleiro/application/dtos/service/ServiceOutputDTO.java
+++ src/main/java/com/example/sistemacabeleleiro/application/dtos/service/ServiceOutputDTO.java
@@ -1,4 +1,7 @@
 package com.example.sistemacabeleleiro.application.dtos.service;
 
-public record ServiceOutputDTO() {
+import com.example.sistemacabeleleiro.domain.entities.service.ServiceStatus;
+
+public record ServiceOutputDTO(int id, String name, String description, double price,
+                               String category, String subcategory, ServiceStatus status) {
 }
--- src/main/java/com/example/sistemacabeleleiro/domain/usecases/service/FindServiceUseCase.java
+++ src/main/java/com/example/sistemacabeleleiro/domain/usecases/service/FindServiceUseCase.java
@@ -1,9 +1,11 @@
 package com.example.sistemacabeleleiro.domain.usecases.service;
 
+import com.example.sistemacabeleleiro.application.dtos.service.ServiceOutputDTO;
 import com.example.sistemacabeleleiro.domain.entities.service.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class FindServiceUseCase {
     private ServiceDAO serviceDAO;
@@ -12,31 +14,36 @@ public class FindServiceUseCase {
         this.serviceDAO = serviceDAO;
     }
 
-    public Optional<Service> findOne(Integer id){
-        if(id == null)
-            throw new IllegalArgumentException("ID can not be null.");
-        return  serviceDAO.findOne(id);
+    public Optional<ServiceOutputDTO> findOne(int id){
+        return  serviceDAO.findOne(id).map(this::mapToDTO);
     }
 
-    public List<Service> findAll(){
-        return serviceDAO.findAll();
+    public List<ServiceOutputDTO> findAll(){
+        List<Service> services = serviceDAO.findAll();
+        return services.stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public List<Service> findByPriceRange(double minPrice, double maxPrice) {
+    public List<ServiceOutputDTO> findByPriceRange(double minPrice, double maxPrice) {
         if (minPrice >= maxPrice)
             throw new IllegalArgumentException("Minimum price can not be greater than maximum price.");
-        return serviceDAO.findByPriceRange(minPrice, maxPrice);
+        return serviceDAO.findByPriceRange(minPrice, maxPrice).stream()
+                .map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public List<Service> findByCategory(String category) {
+    public List<ServiceOutputDTO> findByCategory(String category) {
         if (category == null || category.isEmpty())
             throw new IllegalArgumentException("The category is null or empty.");
-        return serviceDAO.findByCategory(category);
+        return serviceDAO.findByCategory(category).stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public List<Service> findByDiscount(Double discount) {
+    public List<ServiceOutputDTO> findByDiscount(double discount) {
         if (discount == 0.0)
             throw new IllegalArgumentException("This service does not have a discount.");
-        return serviceDAO.findByDiscount(discount);
+        return serviceDAO.findByDiscount(discount).stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
+
+    private ServiceOutputDTO mapToDTO(Service service){
+        return new ServiceOutputDTO(service.getId(),service.getName(),service.getDescription(),
+                service.getPrice(),service.getCategory(),service.getSubCategory(),service.getStatus());
     }
 }
