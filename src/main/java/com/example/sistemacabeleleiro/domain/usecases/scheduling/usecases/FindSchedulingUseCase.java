c7c47041da4a1df248805a277b4f7f13c0596efd
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-06-22 15:53:49 -0300

    refactor: create DTO for find scheduling use case

--- src/main/java/com/example/sistemacabeleleiro/domain/usecases/scheduling/usecases/FindSchedulingUseCase.java
+++ src/main/java/com/example/sistemacabeleleiro/domain/usecases/scheduling/usecases/FindSchedulingUseCase.java
@@ -1,12 +1,14 @@
 package com.example.sistemacabeleleiro.domain.usecases.scheduling.usecases;
 
 import com.example.sistemacabeleleiro.domain.entities.schedulling.Scheduling;
+import com.example.sistemacabeleleiro.domain.usecases.scheduling.dto.SchedulingOutputDTO;
 import com.example.sistemacabeleleiro.domain.usecases.scheduling.repository.SchedulingDAO;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class FindSchedulingUseCase {
     private SchedulingDAO schedulingDAO;
@@ -15,49 +17,52 @@ public class FindSchedulingUseCase {
         this.schedulingDAO = schedulingDAO;
     }
 
-    public Optional<Scheduling> findOne(Integer id){
-        if(id == null){
-            throw new IllegalArgumentException("ID can not be null");
-        }
-        return schedulingDAO.findOne(id);
+    public Optional<SchedulingOutputDTO> findOne(int id){
+        return schedulingDAO.findOne(id).map(this::mapToDTO);
     }
 
-    public List<Scheduling> findByClient(Integer id){
-        if(id == null){
-            throw new IllegalArgumentException("ID can not be null");
-        }
-        return schedulingDAO.findByClient(id);
+    public List<SchedulingOutputDTO> findByClient(int clientId){
+        return schedulingDAO.findByClient(clientId).stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public List<Scheduling> findByEmployee(Integer id){
-        if(id == null){
-            throw new IllegalArgumentException("ID can not be null");
-        }
-        return schedulingDAO.findByEmployee(id);
+    public List<SchedulingOutputDTO> findByEmployee(int employeeId){
+        return schedulingDAO.findByEmployee(employeeId).stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public List<Scheduling> findByService(Integer id){
-        if(id == null){
-            throw new IllegalArgumentException("ID can not be null");
-        }
-        return schedulingDAO.findByService(id);
+    public List<SchedulingOutputDTO> findByService(int id){
+        return schedulingDAO.findByService(id).stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public Optional<Scheduling> findByScheduledDate(LocalDateTime date){
+    public Optional<SchedulingOutputDTO> findByScheduledDate(LocalDateTime date){
         if(date == null){
             throw new IllegalArgumentException("Date can not be null");
         }
-        return schedulingDAO.findByScheduledDate(date);
+        return schedulingDAO.findByScheduledDate(date).map(this::mapToDTO);
     }
 
-    public List<Scheduling> findByTimePeriod(LocalDate startDate, LocalDate endDate){
+    public List<SchedulingOutputDTO> findByTimePeriod(LocalDate startDate, LocalDate endDate){
         if(startDate == null || endDate == null){
             throw new IllegalArgumentException("Dates can not be null");
         }
-        return schedulingDAO.findByTimePeriod(startDate, endDate);
+        return schedulingDAO.findByTimePeriod(startDate, endDate).stream()
+                .map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public List<Scheduling> findAll(){
-        return schedulingDAO.findAll();
+    public List<SchedulingOutputDTO> findAll(){
+        return schedulingDAO.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
+
+    private SchedulingOutputDTO mapToDTO(Scheduling scheduling){
+        return new SchedulingOutputDTO(
+                scheduling.getId(),
+                scheduling.getClient().getId(),
+                scheduling.getClient().getName(),
+                scheduling.getEmployee().getId(),
+                scheduling.getEmployee().getName(),
+                scheduling.getService().getId(),
+                scheduling.getService().getName(),
+                scheduling.getRealizationDate(),
+                scheduling.getStatus()
+        );
     }
 }
