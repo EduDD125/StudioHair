958d20184d5265ca09eef1fe7837805d5a00c8dd
Ariadne <ariadnederezende@gmail.com>
2024-06-24 09:08:52 -0300

    create sqlite scheduling DAO and adjustments in main

--- src/main/java/com/example/sistemacabeleleiro/application/main/Main.java
+++ src/main/java/com/example/sistemacabeleleiro/application/main/Main.java
@@ -3,70 +3,57 @@ package com.example.sistemacabeleleiro.application.main;
 import com.example.sistemacabeleleiro.application.repository.sqlite.SqliteClientDAO;
 import com.example.sistemacabeleleiro.application.repository.sqlite.SqliteEmployeeDAO;
 import com.example.sistemacabeleleiro.application.repository.sqlite.SqliteServiceDAO;
-import com.example.sistemacabeleleiro.domain.usecases.client.dto.ClientInputDTO;
-import com.example.sistemacabeleleiro.domain.usecases.client.dto.ClientOutputDTO;
+import com.example.sistemacabeleleiro.application.repository.sqlite.SqliteSchedulingDAO;
 import com.example.sistemacabeleleiro.domain.usecases.client.repository.ClientDAO;
 import com.example.sistemacabeleleiro.domain.usecases.client.usecases.*;
-import com.example.sistemacabeleleiro.domain.usecases.employee.dto.EmployeeInputDTO;
-import com.example.sistemacabeleleiro.domain.usecases.employee.dto.EmployeeOutputDTO;
-import com.example.sistemacabeleleiro.domain.usecases.service.dto.ServiceInputDTO;
-import com.example.sistemacabeleleiro.domain.usecases.service.dto.ServiceOutputDTO;
-import com.example.sistemacabeleleiro.application.repository.inmemory.InMemoryClientDAO;
-import com.example.sistemacabeleleiro.application.repository.inmemory.InMemoryEmployeeDAO;
-import com.example.sistemacabeleleiro.application.repository.inmemory.InMemorySchedulingDAO;
-import com.example.sistemacabeleleiro.application.repository.inmemory.InMemoryServiceDAO;
-import com.example.sistemacabeleleiro.application.repository.sqlite.DataBaseBuilder;
-import com.example.sistemacabeleleiro.domain.entities.cpf.CPF;
-import com.example.sistemacabeleleiro.domain.entities.email.Email;
-import com.example.sistemacabeleleiro.domain.entities.schedulling.Scheduling;
 import com.example.sistemacabeleleiro.domain.usecases.employee.repository.EmployeeDAO;
 import com.example.sistemacabeleleiro.domain.usecases.employee.usecases.*;
-import com.example.sistemacabeleleiro.domain.usecases.reports.ExportReportUseCase;
-import com.example.sistemacabeleleiro.domain.usecases.reports.GenerateReportUseCase;
-import com.example.sistemacabeleleiro.domain.usecases.scheduling.*;
+import com.example.sistemacabeleleiro.domain.usecases.scheduling.repository.SchedulingDAO;
+import com.example.sistemacabeleleiro.domain.usecases.scheduling.usecases.*;
 import com.example.sistemacabeleleiro.domain.usecases.service.repository.ServiceDAO;
 import com.example.sistemacabeleleiro.domain.usecases.service.usecases.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
+import com.example.sistemacabeleleiro.domain.usecases.reports.ExportReportUseCase;
+import com.example.sistemacabeleleiro.domain.usecases.reports.GenerateReportUseCase;
+import com.example.sistemacabeleleiro.application.repository.sqlite.DataBaseBuilder;
 
 public class Main {
 
     public static CreateClientUseCase createClientUseCase;
-    public  static RemoveClientUseCase removeClientUseCase;
-    public  static FindClientUseCase findClientUseCase;
+    public static RemoveClientUseCase removeClientUseCase;
+    public static FindClientUseCase findClientUseCase;
     public static UpdateClientUseCase updateClientUseCase;
-    public  static ActivateClientUseCase activateClientUseCase;
-    public  static InactivateClientUseCase inactivateClientUseCase;
+    public static ActivateClientUseCase activateClientUseCase;
+    public static InactivateClientUseCase inactivateClientUseCase;
 
-    public  static ActivateEmployeeUseCase activateEmployeeUseCase;
+    public static ActivateEmployeeUseCase activateEmployeeUseCase;
     public static AddEmployeeExpertiseUseCase addEmployeeExpertiseUseCase;
     public static CreateEmployeeUseCase createEmployeeUseCase;
     public static FindEmployeeUseCase findEmployeeUseCase;
     public static InactivateEmployeeUseCase inactivateEmployeeUseCase;
-    public  static RemoveEmployeeUseCase removeEmployeeUseCase;
+    public static RemoveEmployeeUseCase removeEmployeeUseCase;
     public static RemoveExpertiseFromEmployeeUseCase removeExpertiseFromEmployeeUseCase;
-    public  static UpdateEmployeeUseCase updateEmployeeUseCase;
+    public static UpdateEmployeeUseCase updateEmployeeUseCase;
 
-    public  static CancelSchedulingUseCase cancelSchedulingUseCase;
-    public  static CreateSchedulingUseCase createSchedulingUseCase;
-    public  static FindSchedulingUseCase findSchedulingUseCase;
-    public  static UpdateScheduleUseCase updateScheduleUseCase;
+    public static CancelSchedulingUseCase cancelSchedulingUseCase;
+    public static CreateSchedulingUseCase createSchedulingUseCase;
+    public static FindSchedulingUseCase findSchedulingUseCase;
+    public static UpdateScheduleUseCase updateScheduleUseCase;
 
-    public  static CreateServiceUseCase createServiceUseCase;
-    public  static FindServiceUseCase findServiceUseCase;
-    public  static RemoveServiceUseCase removeServiceUseCase;
-    public  static UpdateServiceUseCase updateServiceUseCase;
-    public  static ActivateServiceUseCase activateServiceUseCase;
-    public  static InactivateServiceUseCase inactivateServiceUseCase;
+    public static CreateServiceUseCase createServiceUseCase;
+    public static FindServiceUseCase findServiceUseCase;
+    public static RemoveServiceUseCase removeServiceUseCase;
+    public static UpdateServiceUseCase updateServiceUseCase;
+    public static ActivateServiceUseCase activateServiceUseCase;
+    public static InactivateServiceUseCase inactivateServiceUseCase;
 
-    public  static GenerateReportUseCase generateReportUseCase;
-    public  static ExportReportUseCase exportReportUseCase;
+    public static GenerateReportUseCase generateReportUseCase;
+    public static ExportReportUseCase exportReportUseCase;
 
     public static void main(String[] args) {
         configureInjection();
         setupDatabase();
     }
+
     private static void setupDatabase() {
         DataBaseBuilder dbBuilder = new DataBaseBuilder();
         dbBuilder.buildDataBaseIfMissing();
@@ -78,13 +65,12 @@ public class Main {
         SchedulingDAO schedulingDAO = new SqliteSchedulingDAO();
         ServiceDAO serviceDAO = new SqliteServiceDAO();
 
-        createClientUseCase = new CreateClientUseCase(clientDAO,employeeDAO);
-        removeClientUseCase = new RemoveClientUseCase(clientDAO,schedulingDAO);
+        createClientUseCase = new CreateClientUseCase(clientDAO, employeeDAO);
+        removeClientUseCase = new RemoveClientUseCase(clientDAO, schedulingDAO);
         findClientUseCase = new FindClientUseCase(clientDAO);
         updateClientUseCase = new UpdateClientUseCase(clientDAO);
         activateClientUseCase = new ActivateClientUseCase(clientDAO);
         inactivateClientUseCase = new InactivateClientUseCase(clientDAO);
-
 
         activateEmployeeUseCase = new ActivateEmployeeUseCase(employeeDAO);
         updateEmployeeUseCase = new UpdateEmployeeUseCase(employeeDAO);
@@ -95,13 +81,13 @@ public class Main {
         removeEmployeeUseCase = new RemoveEmployeeUseCase(employeeDAO, schedulingDAO);
         removeExpertiseFromEmployeeUseCase = new RemoveExpertiseFromEmployeeUseCase(employeeDAO, serviceDAO);
 
-        cancelSchedulingUseCase = new CancelSchedulingUseCase(schedulingDAO);
+        cancelSchedulingUseCase = new cncelSchedulingUseCase(schedulingDAO);
         createSchedulingUseCase = new CreateSchedulingUseCase(schedulingDAO, clientDAO, employeeDAO, serviceDAO);
         findSchedulingUseCase = new FindSchedulingUseCase(schedulingDAO);
         updateScheduleUseCase = new UpdateScheduleUseCase(schedulingDAO, clientDAO, employeeDAO, serviceDAO);
+
         inactivateServiceUseCase = new InactivateServiceUseCase(serviceDAO);
         activateServiceUseCase = new ActivateServiceUseCase(serviceDAO);
-
         createServiceUseCase = new CreateServiceUseCase(serviceDAO);
         findServiceUseCase = new FindServiceUseCase(serviceDAO);
         removeServiceUseCase = new RemoveServiceUseCase(serviceDAO, employeeDAO, schedulingDAO);
@@ -109,8 +95,5 @@ public class Main {
 
         generateReportUseCase = new GenerateReportUseCase(schedulingDAO, employeeDAO);
         exportReportUseCase = new ExportReportUseCase(generateReportUseCase);
-
     }
 }
-
-
