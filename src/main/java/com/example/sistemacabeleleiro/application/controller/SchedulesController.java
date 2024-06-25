6d1f9d2b0f6391ddb8e28f92f3607423c12ee6c9
Eduardo Derisso <dudu.derisso@gmail.com>
2024-06-25 18:10:11 -0300

    fixing[schedule fxml]: adicionando funcoes faltantes

--- src/main/java/com/example/sistemacabeleleiro/application/controller/SchedulesController.java
+++ src/main/java/com/example/sistemacabeleleiro/application/controller/SchedulesController.java
@@ -1,8 +1,15 @@
 package com.example.sistemacabeleleiro.application.controller;
 
 import com.example.sistemacabeleleiro.domain.usecases.scheduling.dto.SchedulingOutputDTO;
+import javafx.event.ActionEvent;
 
 public class SchedulesController {
     public void setSchedule(SchedulingOutputDTO selectedSchedule, UIMode mode) {
+    }
+
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
+
+    public void saveOrUpdate(ActionEvent actionEvent) {
     }
 }
